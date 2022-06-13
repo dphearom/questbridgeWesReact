4af61782929b3c95bf619a39642c3b26ba152bd5
Phearom Duong <duongphearom@gmail.com>
2022-06-13 07:56:12 -0400

    latest push

--- src/About/about.js
+++ src/About/about.js
@@ -1,6 +1,7 @@
 import Navbar from "../Navigation/Navbar";
 import Footer from "../Footer/FooterMain";
 import overhead from "./overhead.jpg";
+import profile from "./profile.png"
 
 export default function about() {
   return (
@@ -30,22 +31,43 @@ export default function about() {
             </p>
           </div>
         </div>
-        <div className="mt-10 w-full">
+        <div className="mt-10 w-full pb-12">
           <h1 className="pb-5">Our Members</h1>
-          <div className="flex flex-wrap gap-9 justify-evenly justify-items-center h-12">
-            <div className="shrink-0 border-2 rounded-lg basis-1/4 min-w-0">
-              <h3 className="text-center">Lorep Ipsum</h3>
+          <div className="flex flex-wrap gap-9 justify-evenly justify-items-center">
+            <div className="shrink-0 border-2 rounded-lg basis-1/4 max-w-[15%] min-w-[15%]">
+              <img src={profile} className="h-56 w-full" alt="John Doe"/>
+              <h3 className="text-center text-3xl">John Doe</h3>
+              <p className="text-xs pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                 deserunt mollit anim id est laborum.</p>
             </div>
-            <div className="shrink-0 border-2 rounded-lg basis-1/4 min-w-0">
-                <h3 className="text-center">Lorep Ipsum</h3>
+            <div className="shrink-0 border-2 rounded-lg basis-1/4 max-w-[15%] min-w-[15%]">
+              <img src={profile} className="h-56 w-full" alt="John Doe"/>
+              <h3 className="text-center text-3xl">John Doe</h3>
+              <p className="text-xs pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                 deserunt mollit anim id est laborum.</p>
             </div>
-            <div className="shrink-0 border-2 rounded-lg basis-1/4 min-w-0">
-                <h3 className="text-center">Lorep Ipsum</h3>
+            <div className="shrink-0 border-2 rounded-lg basis-1/4 max-w-[15%] min-w-[15%]">
+              <img src={profile} className="h-56 w-full" alt="John Doe"/>
+              <h3 className="text-center text-3xl">John Doe</h3>
+              <p className="text-xs pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                 deserunt mollit anim id est laborum.</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="pt-20">
+      <div>
         <Footer />
       </div>
     </div>
